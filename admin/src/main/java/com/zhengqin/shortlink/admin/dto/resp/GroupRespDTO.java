1b9222c20547ee95bfbfda26df9223bef9e226af
郑钦 <2892294059@qq.com>
2026-03-26 16:29:13 +0800

    优化查询短链接分组返回实体

--- admin/src/main/java/com/zhengqin/shortlink/admin/dto/resp/GroupRespDTO.java
+++ admin/src/main/java/com/zhengqin/shortlink/admin/dto/resp/GroupRespDTO.java
@@ -1,4 +1,24 @@
 package com.zhengqin.shortlink.admin.dto.resp;
 
+import lombok.Data;
+
+/**
+ * 分组查询返回实体
+ */
+@Data
 public class GroupRespDTO {
+    /**
+     * 分组标识（唯一标识符）
+     */
+    private String gid;
+
+    /**
+     * 分组名称
+     */
+    private String name;
+
+    /**
+     * 分组排序
+     */
+    private Integer sortOrder;
 }
